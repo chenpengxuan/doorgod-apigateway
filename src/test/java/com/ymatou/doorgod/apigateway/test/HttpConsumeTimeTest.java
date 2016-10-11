f6816b689008eef7913de7e9972b92e5fba60932
tuuutuuu <tuwenjie@ymt.corp>
2016-10-11 17:20:56 +0800

    init code

---
+++ src/test/java/com/ymatou/doorgod/apigateway/test/HttpConsumeTimeTest.java
@@ -0,0 +1,45 @@
+package com.ymatou.doorgod.apigateway.test;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.junit.Test;
+
+/**
+ * Created by tuwenjie on 2016/10/11.
+ */
+public class HttpConsumeTimeTest {
+
+    @Test
+    public void test( ) throws Exception {
+        RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(1000)
+                .setSocketTimeout(10000).build();
+
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+        cm.setDefaultMaxPerRoute(10);
+        cm.setMaxTotal(100);
+
+        HttpClient httpClient = HttpClients.custom().setConnectionManager(cm).setDefaultRequestConfig(requestConfig).build();
+
+        HttpPost httpPost = new HttpPost("http://172.16.103.127:8081/10ms1k");
+        HttpResponse response = null;
+        int loops = 5000;
+        long startTime = System.currentTimeMillis();
+        for ( int i=0; i<loops; i++) {
+
+            try {
+
+                response = httpClient.execute(httpPost);
+
+            } finally {
+
+                //System.out.println( response );
+                httpPost.releaseConnection();
+            }
+        }
+        System.out.println("Consumed:" + ((System.currentTimeMillis() - startTime)/loops));
+    }
+}
